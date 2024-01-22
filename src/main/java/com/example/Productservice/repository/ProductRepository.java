1709d0b5ec9c62cbbb940b99bc2263d611889a66
R <2106709@cognizant.com>
2024-01-22 16:49:36 +0530

    22

--- src/main/java/com/example/Productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.productservice.repository;
-
-import com.example.productservice.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
